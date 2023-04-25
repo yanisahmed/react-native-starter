901131f398adef56af0b7d82e8c62fbaa80d9810
Yanis Ahmed <yanisahmed12@gmail.com>
2023-04-25 20:58:21 +0600

    typography added

--- App.js
+++ App.js
@@ -3,11 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 import { colors } from './src/theme/colors';
 import { spacing } from './src/theme/spacing';
 import { useFonts } from 'expo-font';
+import { typography } from './src/theme/typography';
 
 export default function App() {
   const [loaded] = useFonts({
     "Antonio-Medium": require("./assets/fonts/Antonio-Medium.ttf"),
-    "Antonio-Bold": require("./assets/fonts/Antonio-Bold.ttf")
+    "Antonio-Bold": require("./assets/fonts/Antonio-Bold.ttf"),
+    "Spartan-Regular": require("./assets/fonts/Spartan-Regular.ttf"),
+    "Spartan-Bold": require("./assets/fonts/Spartan-Bold.ttf")
   })
 
   if(!loaded){
@@ -17,8 +20,10 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
-      <Text style={{fontFamily: "Antonio-Medium", fontSize: spacing[5]}}>Hello!</Text>
-      <Text style={{ marginTop: spacing[4]}}>Hello Again!</Text>
+      <Text style={{fontFamily: typography.primary, fontSize: spacing[5]}}>Hello!</Text>
+      <Text style={{ marginTop: spacing[4], fontFamily: typography.primaryBold, fontSize: spacing[4]}}>Hello Again!</Text>
+      <Text style={{ marginTop: spacing[4], fontFamily: typography.bold, fontSize: spacing[4]}}>Hello Again, Yanis!</Text>
+
       <StatusBar style="auto" />
     </View>
   );
