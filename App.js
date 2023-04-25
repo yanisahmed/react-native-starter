c0dd85648f4dce69d63989d001d115213934cfea
Yanis Ahmed <yanisahmed12@gmail.com>
2023-04-25 18:10:04 +0600

    local fonts added

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { colors } from './src/theme/colors';
+import { spacing } from './src/theme/spacing';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [loaded] = useFonts({
+    "Antonio-Medium": require("./assets/fonts/Antonio-Medium.ttf"),
+    "Antonio-Bold": require("./assets/fonts/Antonio-Bold.ttf")
+  })
+
+  if(!loaded){
+    return <Text>
+      Font is loading...
+    </Text>
+  }
   return (
     <View style={styles.container}>
-      <Text>Hello, Yanis!</Text>
+      <Text style={{fontFamily: "Antonio-Medium", fontSize: spacing[5]}}>Hello!</Text>
+      <Text style={{ marginTop: spacing[4]}}>Hello Again!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +27,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f6f6f6',
+    backgroundColor: colors.darkOrange,
     alignItems: 'center',
     justifyContent: 'center',
   },
