6cf6e9ed3b74478a19cfe912001c3b1cc2390d99
Yanis Ahmed <yanisahmed12@gmail.com>
2023-04-25 13:02:33 +0600

    colors file added

---
+++ src/theme/colors.js
@@ -0,0 +1,14 @@
+export const colors = {
+    black: '#070724',
+    white: '#ffffff',
+    darkGrey: '#38384f',
+    grey: '#838391',
+    lightGreen: '#419ebb',
+    orange: '#eda249',
+    purple: '#6f2e26',
+    darkOrange: '#d14c32',
+    red: '#d83a34',
+    lightRed: '#cd5120',
+    green: '#lec2a4',
+    blue: '#2d68f0'
+}
